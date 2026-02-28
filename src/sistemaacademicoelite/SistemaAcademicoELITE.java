c87882422c11d4118d4ce37310be1d0106f6044f
Daniela Rojas Diaz <drojasd@unicesar.edu.co>
2026-02-28 17:50:51 -0500

    feat: crear estructura base de Main con ArrayList

--- src/sistemaacademicoelite/SistemaAcademicoELITE.java
+++ src/sistemaacademicoelite/SistemaAcademicoELITE.java
@@ -4,17 +4,27 @@
  */
 package sistemaacademicoelite;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  *
  * @author DANIELA ROJAS
  */
 public class SistemaAcademicoELITE {
 
+    private static ArrayList<Estudiante> estudiantes = new ArrayList<>();
+    private static ArrayList<Asignatura> asignaturas = new ArrayList<>();
+    private static ArrayList<Nota> notas = new ArrayList<>();
     /**
      * @param args the command line arguments
      */
+    
+    private static Scanner sc = new Scanner (System.in);
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    
+    
     
 }
